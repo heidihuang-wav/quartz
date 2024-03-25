03fcbebb67b77fd96998e25126fd5a8e8ad7e615
heidihuang-wav <132696198+heidihuang-wav@users.noreply.github.com>
2024-03-25 16:27:32 -0700

    change favicon file

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://heidihuang.com",
+    baseUrl: "heidihuang.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
